37e27fcae3795798fdb2af178ce50ed3c732c72a
hanraeul <toast17@naver.com>
2024-04-29 20:58:37 +0900

    양의정수 입력, 사칙연산 전달

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -1,7 +1,22 @@
-package Calculator;
+package Calculator;//package Calculator;
 
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        Scanner scc
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요");
+        int Number1 = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요");
+        int Number2 = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char text = sc.nextLine().charAt(0);
+
+        System.out.println(Number1 + Number2);
+
     }
+
 }
