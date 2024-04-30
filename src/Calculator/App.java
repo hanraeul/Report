c35772b9faa118d4fd9c12ae3ef46a00034ec582
hanraeul <toast17@naver.com>
2024-04-30 17:08:13 +0900

    1-4 무한 계산 exit로 끝내기

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -4,20 +4,37 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String Out = null;
 
-        System.out.println("첫 번째 숫자를 입력하세요");
-        int Number1 = sc.nextInt();
-        System.out.println("두 번째 숫자를 입력하세요");
-        int Number2 = sc.nextInt();
+        do {
+                System.out.println("첫 번째 숫자를 입력하세요");
+                int Number1 = sc.nextInt();
+                System.out.println("두 번째 숫자를 입력하세요");
+                int Number2 = sc.nextInt();
 
-        sc.nextLine();
+                sc.nextLine();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char Text = sc.nextLine().charAt(0);
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char Text = sc.nextLine().charAt(0);
 
-        double result = calculate( Number1,  Number2,  Text);
+                double result = calculate(Number1, Number2, Text);
 
-        System.out.println("결과 : " + result);
+                System.out.println("결과 : " + result);
+
+                System.out.println("더 계산하시겠습니까? (exit 입력시 종료)");
+                Out = sc.next();
+        }
+        while (!(Out.equals("exit")||Out.equals("EXIT")));
+
+
+
+
+
+
+
+
+
+
 
     }
 
