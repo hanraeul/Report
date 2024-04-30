5f43d8616e81701f20f904132d90fb01901195de
hanraeul <toast17@naver.com>
2024-04-30 16:27:58 +0900

    1-3 사칙연산 결과값 전달

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -13,10 +13,39 @@ public class App {
         sc.nextLine();
 
         System.out.print("사칙연산 기호를 입력하세요: ");
-        char text = sc.nextLine().charAt(0);
+        char Text = sc.nextLine().charAt(0);
 
-        System.out.println(Number1 + Number2);
+        double result = calculate( Number1,  Number2,  Text);
+
+        System.out.println("결과 : " + result);
 
     }
+
+    public static double calculate(int num1, int num2, char text) {
+        int result = 0;
+        switch (text) {
+            case '+' :
+                result = num1 + num2;
+                break;
+            case '-' :
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/' :
+                if(num2 != 0) {
+                    result = num1 / num2;
+                }
+                else {
+                    System.out.println("0이 입력될 수 없습니다.");
+                }
+                break;
+        }
+
+
+        return result;
+    }
+
 
 }
